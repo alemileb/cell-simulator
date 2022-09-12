053d827fe3dd6328749e0afe13b96e71c95692af
Alexandra S <alexandra.santos@team.telstra.com>
2022-09-12 12:10:40 +1000

    Cell simulator functionality plus some unit testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import enzyme from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+configure({ adapter: new Adapter() });
+
+// Wrapper will know the type of the component.
+const wrapper = enzyme.shallow(<App />);
+
+describe('Cell board rendering', () => {
+  let component = wrapper.find('.matrix');
+  it('should render the cell board', () => {
+    
+    expect(component.length).toBe(1);
+    expect(component.find('Row').length).toBe(8);
+    expect(component.find('Col').length).toBe(64);
+    expect(component.find('Col.alive').length).toBe(0);
+  });
+
+  it('should toggle cell status', () => {
+    //set a cell alive
+    let secondRow = component.find('Row').at(1);
+    secondRow.find('Col').at(0).simulate("click");
+    wrapper.update();
+    secondRow = wrapper.find('.matrix').find('Row').at(1);
+    expect(secondRow.find('Col.alive').length).toBe(1);
+    
+     //set a cell dead
+    secondRow.find('Col').at(0).simulate("click");
+    wrapper.update();
+    secondRow = wrapper.find('.matrix').find('Row').at(1);
+    expect(secondRow.find('Col.alive').length).toBe(0);
+  });
+
+
+  describe('Cell simulation', () => {
+    let component = wrapper.find('.matrix');
+    it('next generation should be same as initial state', () => {
+      
+      //set cells alive 2 x 2 square block
+      /*
+      let secondRow = component.find('Row').at(1);
+      secondRow.find('Col').at(1).props().onClick();
+      wrapper.update();
+      secondRow.find('Col').at(2).props().onClick();
+      wrapper.update();
+      let thirdRow = component.find('Row').at(2);
+      thirdRow.find('Col').at(1).props().onClick();
+      
+      wrapper.update();
+      thirdRow.find('Col').at(2).props().onClick();
+      wrapper.update()
+      console.log(wrapper.find('.matrix').find('Row').at(2).debug());
+      expect(wrapper.find('.matrix').find('Col.alive').length).toBe(4);
+      */
+    });
+  })
+  
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import _ from 'lodash';
 
 function App() {
+  const cols: number = 8;
+  const rows: number = 8;
+  let initialMatrix: boolean[][] = [];
+  
+  //Initiates an empty board/matrix
+  for (let i = 0; i < rows; i++) {
+    initialMatrix[i] = [];
+    for (let j = 0; j < cols; j++) {
+      initialMatrix[i][j] = false;
+    }
+  }
+
+  const [matrix, setMatrix] = useState(initialMatrix);
+  const [animation, setAnimation] = useState(false);
+
+  //Toggles the status (live/dead) of a cell 
+  const setCellAlive = (x: number, y: number) => {
+    let newMatrix = _.cloneDeep(matrix);
+    newMatrix[x][y] = !newMatrix[x][y];
+    setMatrix(newMatrix);
+  };
+
+  //Count alive neighbours for a cell given its x and y position
+  const countNeighbourCells = (x: number, y: number) => {
+    let total = 0;
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (
+          !(i === 0 && j === 0) && //doesnt include the current cell
+          !(x === 0 || y === 0 || x === cols - 1 || y === rows - 1)
+        ) {
+          total += matrix[x + j][y + i] ? 1 : 0;
+        }
+      }
+    }
+    return total;
+  };
+
+  //Excecutes next generation of cells
+  const nextGeneration = (currentMatrix: boolean[][]) => {
+    let newMatrix = _.cloneDeep(currentMatrix);
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        let adjacentsCells = countNeighbourCells(x, y);
+        newMatrix[x][y] =
+          (newMatrix[x][y] && adjacentsCells === 2) || adjacentsCells === 3;
+      }
+    }
+    setMatrix(newMatrix);
+  };
+
+  
+  //Executes when animation has started and when the matrix is updated
+  useEffect(() => {
+    let interval = 0;
+    if (animation) { 
+       interval = window.setInterval(() => {
+        nextGeneration(matrix)
+      }, 2000);
+    }
+    else {
+      clearInterval(interval);
+    }    
+    return () => clearInterval(interval);
+    //eslint-disable-next-line
+  }, [animation, matrix]);
+
+  // starts simulation and update it every 2 seconds
+  const startSimulation = () =>  setAnimation(true);
+
+  //stops the simulation
+  const stopSimulation = () =>  setAnimation(false);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='matrix'>
+        {matrix.map((row, i) => {
+          return (
+            <Row key={`row-${i}`} className='no-gutters'>
+              {row.map((col, j) => {
+                return (
+                  <Col
+                    md='auto'
+                    key={`col-${i}-${j}`}
+                    className={`${col ? 'alive' : ''}`}
+                    onClick={() => setCellAlive(i, j)}
+                  >
+                    <div className='cell'></div>
+                  </Col>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </div>
+      <br></br>
+      <Container>
+        <Row>
+        { !animation &&  
+          <Col md='2'>
+            <input
+              type='button'
+              value='start'
+              onClick={() => startSimulation()}
+            />
+          </Col>
+        }
+          { animation && 
+          <Col md='2'>
+            <input
+              type='button'
+              value='stop'
+              onClick={() => stopSimulation()}
+            />
+          </Col>
+          }
+          <Col md='2'>
+            <input
+              type='button'
+              value='clear'
+              onClick={() => setMatrix(initialMatrix)}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
